ef5613cfb946c8226b81dc74d278d12f87c13ff6
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2020-10-20 12:24:49 +0600

    Create add_active_class_in_first_row.vue

---
+++ add_active_class_in_first_row.vue
@@ -0,0 +1,21 @@
+<div v-for="(servicedetails,key,index) in getServices" :class="['tab-pane fade',(key == 0 ? 'show active':' ')]" :id="'pills-home'+servicedetails.id" role="tabpanel">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="panel_content text-center">
+                                        <img :src="'public/images/services/'+servicedetails.image" style="width: 200px;height: auto;"
+                                            alt="image">
+                                    </div>
+                                </div>
+                                <div class="col-sm-8">
+                                    <div class="panel_descrip">
+                                        <h4>{{servicedetails.name}}</h4>
+                                        <ul>
+                                            <li class="sub_panel" v-for="details in servicedetails.details"><i class="fas fa-check"></i>
+                                                {{details}}
+                                            </li>
+                                          
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
