7d4ebee7e2ba24cc097e31032d130991e1efb9d2
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2020-10-21 13:29:10 +0600

    Create page_dose_not_change_dynamacaly.vue

---
+++ page_dose_not_change_dynamacaly.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "Projects",
+  data() {
+    return {
+      projects: [],
+    };
+  },
+
+  mounted() {
+    this.getProject();
+  },
+  watch: {
+    $route(to, from) {
+      this.getProject();
+    },
+  },
+  methods: {
+    getProject() {
+      let id = this.$route.params.id;
+
+      axios.get("/projectsdetails/" + id).then((response) => {
+        this.projects = response.data.data;
+        console.log(response);
+      });
+    },
+  },
+};
+</script>
