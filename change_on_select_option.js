bf6ab4e8a99f0b33394cef41b1ad12b4deb8a1c0
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2020-10-25 11:38:58 +0600

    Create change_on_select_option.js

---
+++ change_on_select_option.js
@@ -0,0 +1,5 @@
+        <select class="form-control" id="sfslf" @change="onChange($event)" v-model="key">
+                                        <option selected value="1">Reguler</option>
+                                        <option value="2">Premium</option>
+
+                                    </select>
