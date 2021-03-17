dce4d1d7703c1fd58488bc305a6d3b9891db737a
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2021-03-17 22:34:05 +0600

    Create get_data_from_vuex.vue

---
+++ doc/get_data_from_vuex.vue
@@ -0,0 +1,8 @@
+ mounted(){
+         return this.$store.dispatch('allProducts');
+      },
+      computed:{
+         getAllProduct(){
+            return this.$store.getters.getAllProduct;
+         }
+      }
