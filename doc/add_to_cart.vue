cccd7112b018081b73cf29ecff377ab052f810c3
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2021-03-22 11:00:40 +0600

    Create add_to_cart.vue

---
+++ doc/add_to_cart.vue
@@ -0,0 +1,43 @@
+
+## In Product.vue Page
+
+addtocart(product){
+    return this.$store.dispatch('addtocart',{
+       product:product,
+       qty:1
+    });
+ }
+ 
+ 
+ 
+## In store.js at action section
+
+addtocart({ commit }, { product, qty }) {
+			commit('ADD_TO_CART', { product, qty })
+
+		},
+
+## In store.js at mutations section
+
+	ADD_TO_CART(state, { product, qty }) {
+			state.carts.push({
+				product,
+				qty
+			})
+      
+
+## In store.js at state section
+state: {
+		
+		carts: [],
+		
+	},
+  
+  ## In cart.vue section
+  
+  computed:{
+         carts(){
+            return this.$store.state.carts;
+         },
+      }
+ 
